0e85ea3e1be3e3e64c2f207dd80e768af5a79ae3
Bojan <culibrkbojan98@gmail.com>
2020-09-21 20:17:48 +0200

    Registracija provera

--- JavaBLD/src/java/servleti/ServletRegistracija.java
+++ JavaBLD/src/java/servleti/ServletRegistracija.java
@@ -5,12 +5,39 @@
  */
 package servleti;
 
+import entity.Korisnici;
+import java.io.File;
+import java.sql.*;
+import javax.servlet.http.*;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import net.sf.ehcache.hibernate.HibernateUtil;
+import org.hibernate.Query;
+import org.hibernate.SQLQuery;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileItemFactory;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.io.FilenameUtils;
+import org.hibernate.HibernateException;
+import org.hibernate.transform.Transformers;
 
 /**
  *
@@ -29,19 +56,6 @@ public class ServletRegistracija extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet ServletRegistracija</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet ServletRegistracija at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
@@ -70,7 +84,140 @@ public class ServletRegistracija extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        /*
+        String ime;
+        String prezime;
+        String user;
+        String pass;
+        
+        ime = request.getParameter("ime");
+        prezime = request.getParameter("prezime");
+        user = request.getParameter("korisnicko");
+        pass = request.getParameter("sifra");
+        
+        SessionFactory sf = new Configuration().configure().buildSessionFactory();
+        Session s = sf.openSession();
+        s.beginTransaction();
+        
+        Korisnici k = new Korisnici();
+        */
+        
+        
+        
+        String filePath = "";
+        String nazivF = "";
+        ArrayList<String> podaci = new ArrayList<String>();
+        
+        boolean isMultipart = ServletFileUpload.isMultipartContent(request);
+
+        if (isMultipart)
+        {
+            FileItemFactory factory = new DiskFileItemFactory();
+            ServletFileUpload upload = new ServletFileUpload(factory);
+
+            try
+            {
+                List items = upload.parseRequest(request);
+                Iterator iterator = items.iterator();
+                while (iterator.hasNext())
+                {
+                    FileItem item = (FileItem) iterator.next();
+
+                    if (!item.isFormField())
+                    {
+                        String fileName = item.getName();
+                        
+                        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
+                        LocalDateTime now = LocalDateTime.now();  
+                        String time = now.format(dtf);
+                        String str = time.replace("/", "");
+                        String str0 = str.replace(":", "");
+                        String str1 = str0.replaceAll("\\s+","");
+                        str1+=fileName;
+
+                        String root = getServletContext().getRealPath("/");
+                        File path = new File(root + "/images");
+                        if (!path.exists())
+                        {
+                            boolean status = path.mkdirs();
+                        }
+
+                        File uploadedFile = new File(path + "/" + str1);
+                        filePath = uploadedFile.getCanonicalPath();
+                        nazivF = uploadedFile.getName();
+
+                        item.write(uploadedFile);
+                    }
+                    else
+                    {
+                        podaci.add(item.getString());
+                    }
+                }
+            }
+            catch (FileUploadException e)
+            {
+                String errormsg = e.getMessage();
+                request.setAttribute("errormsg", errormsg);
+                request.getRequestDispatcher("error.jsp").forward(request, response);
+            }
+            catch (Exception e) {
+                String errormsg = e.getMessage();
+                request.setAttribute("errormsg", errormsg);
+                request.getRequestDispatcher("error.jsp").forward(request, response);
+            }
+        }
+        
+        String imgpa = "images/";
+        
+        if(!(nazivF.equals("images")))
+        {
+            
+            imgpa+=nazivF;
+        }
+        
+        try
+        {
+            SessionFactory sf = new Configuration().configure().buildSessionFactory();
+            Session s = sf.openSession();
+            Transaction tr = s.beginTransaction();
+            
+            SQLQuery q=s.createSQLQuery("select * from korisnici").addEntity("korisnici",Korisnici.class);
+            
+            List<Korisnici> rows = q.list();
+            for(Korisnici row:rows)
+            {
+                if(row.getUsername().equals(podaci.get(2)))
+                {
+                    request.setAttribute("zauzetoIme", "Korisničko ime je zauzeto.");
+                    request.getRequestDispatcher("registracija.jsp").forward(request, response);
+                }
+            }
+        }
+        catch(HibernateException ex)
+        {
+            
+        }
+        
+        
+        try
+        {
+            SessionFactory sf = new Configuration().configure().buildSessionFactory();
+            Session s = sf.openSession();
+            Transaction tr = s.beginTransaction();
+
+            SQLQuery q=s.createSQLQuery("insert into korisnici(imgPath,ime,prezime,username,password,uloga)"
+                    + "VALUES('"+imgpa+"', '"+podaci.get(0)+"', '"+podaci.get(1)+"', '"+podaci.get(2)+"', '"+podaci.get(3)+"', 'klijent')");
+            q.executeUpdate();
+            tr.commit();
+            s.close();
+        }
+        catch(HibernateException ex)
+        {
+            String errormsg = ex.getMessage();
+            request.setAttribute("errormsg", errormsg);
+            request.getRequestDispatcher("error.jsp").forward(request, response);
+        }
+        
     }
 
     /**
--- JavaBLD/web/error.jsp
+++ JavaBLD/web/error.jsp
@@ -3,12 +3,11 @@
 	Future Imperfect by HTML5 UP
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
--->
-<%@page contentType="text/html" pageEncoding="UTF-8"%>
+--><%@page contentType="text/html" pageEncoding="UTF-8"%>
 <html>
 	<head>
 		<title>Single - Future Imperfect by HTML5 UP</title>
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
 		<link rel="stylesheet" href="assets/css/main.css" />
 	</head>
@@ -87,48 +86,28 @@
 						<!-- Actions -->
 							<section>
 								<ul class="actions stacked">
-									<li><a href="#" class="button large fit" style="color:#fff;">Prijava</a></li>
+									<li><a href="#" class="button large fit">Prijava</a></li>
 								</ul>
 							</section>
 
 					</section>
-                                
-                                <script>
-                                    function readURL(input) {
-                                        if (input.files && input.files[0]) {
-                                            var reader = new FileReader();
-
-                                            reader.onload = function (e) {
-                                                $('#pic')
-                                                    .attr('src', e.target.result)
-                                                    .width(250)
-                                                    .height(250);
-                                            };
-
-                                            reader.readAsDataURL(input.files[0]);
-                                        }
-                                    }
-                                </script>
 
 				<!-- Main -->
 					<div id="main">
-                                                <form method="post" action="ServletRegistracija" class="post" style="width:50%;height: 100%; margin: auto;-webkit-box-shadow: 3px 3px 7px -1px rgba(18,19,30,0.54); -moz-box-shadow: 3px 3px 7px -1px rgba(18,19,30,0.54); box-shadow: 3px 3px 7px -1px rgba(18,19,30,0.54);" enctype="multipart/form-data">
-                                                    <div style="float:right;">
-                                                        <label style="color:#aab0c1;">Korisnička slika</label>
-                                                        <input type="file" name="file" onChange="readURL(this)"><br><br>
-                                                        <img id="pic" src="#" alt="" style="border-radius: 50%;"/>
-                                                    </div>
-                                                    <div>
-                                                        <input type="text" name="ime" placeholder="Ime" style="width: 40%; margin:0 !important;"><br>
-                                                        <input type="text" name="prezime" placeholder="Prezime" style="width: 40%;"><br>
-                                                        <input type="text" name="korisnicko" placeholder="Korisničko ime" style="width: 40%;"><br>
-                                                        <input type="password" name="sifra" placeholder="Šifra" style="width: 40%;"><br>
-                                                        <input type="password" name="sifrap" placeholder="Potvrda šifre" style="width: 40%;"><br>
-                                                    </div>
-                                                    
-                                                    <input type="submit" value="Registruj se">
-                                                    <input type="reset" value="Poništi">
-                                                </form>
+
+						<!-- Post -->
+							<article class="post" style="max-width: 50%; margin: auto;">
+                                                            <h2 style="color: red;text-align: center;">
+                                                                GREŠKA
+                                                            </h2>
+                                                            <h3 style="text-align: center;font-size: 0.5em;">
+                                                                 <%
+                                                                     String msg = (String)request.getAttribute("errormsg");
+                                                                     if(msg!=null)
+                                                                     {%><%= msg%><%}
+                                                                  %>
+                                                            </h3>
+							</article>
 
 					</div>
 
